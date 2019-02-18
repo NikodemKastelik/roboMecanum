0790cd94c00a9f0411185fe1ab8760bbaa22518d
Nikodem Kastelik <nikodemkastelik@gmail.com>
2019-02-18 20:18:55 +0100

    NK-NONE: Add PID control for one robot motor

---
+++ inc/pinout.h
@@ -0,0 +1,113 @@
+#ifndef _PINOUT_H_
+#define _PINOUT_H_
+
+#define __GLUE3(x, y, z) x ## y ## z
+#define _GLUE3(x, y, z)  __GLUE3(x, y, z)
+
+#define __GLUE(x, y) x ## y
+#define _GLUE(x, y)  __GLUE(x, y)
+
+#define HAL_RCC_GPIOE  HAL_RCC_GPIO
+
+#define LED_PIN_PORT    GPIOD
+#define LED_PIN_GREEN   12
+#define LED_PIN_ORANGE  13
+#define LED_PIN_RED     14
+#define LED_PIN_BLUE    15
+
+#define VCP_USART_IDX   2
+#define VCP_USART       _GLUE(USART, VCP_USART_IDX)
+#define VCP_USART_RCC   _GLUE(HAL_RCC_USART, VCP_USART_IDX)
+#define VCP_USART_IRQn  _GLUE3(USART, VCP_USART_IDX, _IRQn)
+#define VCP_PIN_PORT    GPIOA
+#define VCP_PIN_AF      HAL_GPIO_AF_7
+#define VCP_PIN_TX      2
+#define VCP_PIN_RX      3
+
+#define WALLCLOCK_TIMER_IDX    7
+#define WALLCLOCK_TIMER        _GLUE(TIM, WALLCLOCK_TIMER_IDX)
+#define WALLCLOCK_TIMER_RCC    _GLUE(HAL_RCC_TIM, WALLCLOCK_TIMER_IDX)
+#define WALLCLOCK_TIMER_IRQn   _GLUE3(TIM, WALLCLOCK_TIMER_IDX, _IRQn)
+#define WALLCLOCK_TIMER_FREQ   HAL_TIM_FREQ_100kHz
+#define WALLCLOCK_TIMER_TICKS  3125
+#define wallclock_handler      _GLUE3(TIM, WALLCLOCK_TIMER_IDX, _Handler)
+
+/*
+ * @brief Symbols for motor steering PWM.
+ *
+ * Each PWM occupies one advanced timer. Each timer consists of four channels.
+ * To control one motor two channels are needed. One channel controls clockwise
+ * rotation while second channel controls counter clockwise rotation.
+ */
+#define PWM_FRONT_TIMER_IDX  1
+#define PWM_FRONT_TIMER      _GLUE(TIM, PWM_FRONT_TIMER_IDX)
+#define PWM_FRONT_TIMER_RCC  _GLUE(HAL_RCC_TIM, PWM_FRONT_TIMER_IDX)
+#define PWM_FRONT_PIN_PORT   GPIOE
+#define PWM_FRONT_PIN_AF     HAL_GPIO_AF_1
+#define PWM_FR_PIN_CH_CW     9
+#define PWM_FR_PIN_CH_CCW    11
+#define PWM_FL_PIN_CH_CW     13
+#define PWM_FL_PIN_CH_CCW    14
+#define PWM_FR_CH_CW         HAL_TIM_CH1
+#define PWM_FR_CH_CCW        HAL_TIM_CH2
+#define PWM_FL_CH_CW         HAL_TIM_CH3
+#define PWM_FL_CH_CCW        HAL_TIM_CH4
+
+#define PWM_REAR_TIMER_IDX   8
+#define PWM_REAR_TIMER       _GLUE(TIM, PWM_REAR_TIMER_IDX)
+#define PWM_REAR_TIMER_RCC   _GLUE(HAL_RCC_TIM, PWM_REAR_TIMER_IDX)
+#define PWM_REAR_PIN_PORT    GPIOC
+#define PWM_REAR_PIN_AF      HAL_GPIO_AF_3
+#define PWM_RR_PIN_CH_CW     6
+#define PWM_RR_PIN_CH_CCW    7
+#define PWM_RL_PIN_CH_CW     8
+#define PWM_RL_PIN_CH_CCW    9
+#define PWM_RR_CH_CW         HAL_TIM_CH1
+#define PWM_RR_CH_CCW        HAL_TIM_CH2
+#define PWM_RL_CH_CW         HAL_TIM_CH3
+#define PWM_RL_CH_CCW        HAL_TIM_CH4
+
+#define PWM_TICKS  1000
+
+/*
+ * @brief Symbols for motor encoder control.
+ *
+ * Each motor has its own separate incremental encoder. Each encoder instance
+ * occupies one timer instance. To read encoder two channels are needed.
+ */
+#define ENC_FR_TIMER_IDX     2
+#define ENC_FR_TIMER         _GLUE(TIM, ENC_FR_TIMER_IDX)
+#define ENC_FR_TIMER_RCC     _GLUE(HAL_RCC_TIM, ENC_FR_TIMER_IDX)
+#define ENC_FR_PIN_CHA_PORT  GPIOA
+#define ENC_FR_PIN_CHB_PORT  GPIOB
+#define ENC_FR_PIN_AF        HAL_GPIO_AF_1
+#define ENC_FR_PIN_CHA       15
+#define ENC_FR_PIN_CHB       3
+
+#define ENC_FL_TIMER_IDX     3
+#define ENC_FL_TIMER         _GLUE(TIM, ENC_FL_TIMER_IDX)
+#define ENC_FL_TIMER_RCC     _GLUE(HAL_RCC_TIM, ENC_FL_TIMER_IDX)
+#define ENC_FL_PIN_PORT      GPIOB
+#define ENC_FL_PIN_AF        HAL_GPIO_AF_2
+#define ENC_FL_PIN_CHA       4
+#define ENC_FL_PIN_CHB       5
+
+#define ENC_RR_TIMER_IDX     4
+#define ENC_RR_TIMER         _GLUE(TIM, ENC_RR_TIMER_IDX)
+#define ENC_RR_TIMER_RCC     _GLUE(HAL_RCC_TIM, ENC_RR_TIMER_IDX)
+#define ENC_RR_PIN_PORT      GPIOB
+#define ENC_RR_PIN_AF        HAL_GPIO_AF_2
+#define ENC_RR_PIN_CHA       6
+#define ENC_RR_PIN_CHB       7
+
+#define ENC_RL_TIMER_IDX     5
+#define ENC_RL_TIMER         _GLUE(TIM, ENC_RL_TIMER_IDX)
+#define ENC_RL_TIMER_RCC     _GLUE(HAL_RCC_TIM, ENC_RL_TIMER_IDX)
+#define ENC_RL_PIN_PORT      GPIOA
+#define ENC_RL_PIN_AF        HAL_GPIO_AF_2
+#define ENC_RL_PIN_CHA       0
+#define ENC_RL_PIN_CHB       1
+
+#define ENC_TICKS_PER_REV       4480
+
+#endif // _PINOUT_H_
--- src/main.c
+++ src/main.c
@@ -12,121 +12,66 @@
 #include <logger.h>
 #include <pid.h>
 
-#define LED_ORANGE_PIN  13
-#define LED_GREEN_PIN   12
-#define LED_PIN_PORT    GPIOD
-#define LED_PIN_RCC     HAL_RCC_GPIOD
-
-#define VCP_USART       USART2
-#define VCP_USART_RCC   HAL_RCC_USART2
-#define VCP_USART_IRQn  USART2_IRQn
-#define VCP_PINS_PORT   GPIOA
-#define VCP_PINS_RCC    HAL_RCC_GPIOA
-#define VCP_AF_NUM      HAL_GPIO_AF_7
-#define VCP_TX_PIN      2
-#define VCP_RX_PIN      3
-
-#define PWM_TIMER       TIM3
-#define PWM_TIMER_RCC   HAL_RCC_TIM3
-#define PWM_PINS_PORT   GPIOC
-#define PWM_PINS_RCC    HAL_RCC_GPIOC
-#define PWM_AF_NUM      HAL_GPIO_AF_2
-#define PWM_CH1_PIN     6
-#define PWM_CH2_PIN     7
-#define PWM_CH3_PIN     8
-#define PWM_CH4_PIN     9
-#define PWM_TICKS       1000
-
-#define ENC1_TIMER      TIM4
-#define ENC1_TIMER_RCC  HAL_RCC_TIM4
-#define ENC1_TIMER_IRQn TIM4_IRQn
-#define ENC1_PINS_PORT  GPIOB
-#define ENC1_PINS_RCC   HAL_RCC_GPIOB
-#define ENC1_CH_A_PIN   6
-#define ENC1_CH_B_PIN   7
-#define ENC1_AF_NUM     HAL_GPIO_AF_2
-#define enc1_handler    TIM4_Handler
-
-#define ENC_TICKS_PER_REV       4480
-#define ENC_TICKS_PER_DEGREE    (ENC_TICKS_PER_REV / 360)
-#define ENC_TICKS_TO_DEGREE(x)  ((x) / ENC_TICKS_PER_DEGREE)
-
-#define SPEED_SENSE_FREQ        HAL_TIM_FREQ_2MHz
-
-#define HAL_RCC_GPIOE           HAL_RCC_GPIO
-#define SPEED_SENSE1_TIMER      TIM9
-#define SPEED_SENSE1_TIMER_RCC  HAL_RCC_TIM9
-#define SPEED_SENSE1_TIMER_IRQn TIM1_BRK_TIM9_IRQn
-#define SPEED_SENSE1_PINS_PORT  GPIOE
-#define SPEED_SENSE1_PINS_RCC   HAL_RCC_GPIOE
-#define SPEED_SENSE1_AF_NUM     HAL_GPIO_AF_3
-#define SPEED_SENSE1_CH1_PIN    5
-#define SPEED_SENSE1_CH2_PIN    6
-#define speed_sense1_handler    TIM1_BRK_TIM9_Handler
-
-#define WALLCLOCK_TIMER        TIM7
-#define WALLCLOCK_TIMER_RCC    HAL_RCC_TIM7
-#define WALLCLOCK_TIMER_IRQn   TIM7_IRQn
-#define WALLCLOCK_TIMER_FREQ   HAL_TIM_FREQ_100kHz
-#define WALLCLOCK_TIMER_TICKS  3125
-#define wallclock_handler      TIM7_Handler
-
-#define BUTTON_PORT  GPIOA
-#define BUTTON_PIN   0
-
-#define MOTOR_DIR_PINS_PORT  GPIOC
-#define MOTOR_DIR_PINS_RCC   HAL_RCC_GPIOC
-#define MOTOR_DIR_CW_PIN     14
-#define MOTOR_DIR_CCW_PIN    15
+#include <pinout.h>
 
 #define STR_TO_PROCESS_SIZE  64
 #define STRX_MNGR_BUFSIZE  128
 
-pid_t pid1;
+pid_t pid_fr;
 
 char strx_mngr_buffer[STRX_MNGR_BUFSIZE];
 strx_mngr_t strx_mngr;
 
-drv_usart_t usart2  = DRV_USART_INSTANCE_GET(2);
+drv_usart_t vcp_usart  = DRV_USART_INSTANCE_GET(2);
 uint8_t recv_byte;
 
-volatile int32_t absolute_pos;
+volatile bool pid_proced;
+volatile int32_t new_pwm_signal;
+
+void dwt_init()
+{
+    CoreDebug->DEMCR |= CoreDebug_DEMCR_TRCENA_Msk;
+    DWT->CTRL |= DWT_CTRL_CYCCNTENA_Msk;
+}
+
+void dwt_delay_us(uint32_t us)
+{
+    uint32_t target_tick = DWT->CYCCNT + us * (16000000 / 1000000);
+    while (DWT->CYCCNT <= target_tick);
+}
+
+void motor_set_pwm(int32_t pwm_signal)
+{
+    hal_tim_cc_set(PWM_FRONT_TIMER, PWM_FR_CH_CW, 0);
+    hal_tim_cc_set(PWM_FRONT_TIMER, PWM_FR_CH_CCW, 0);
+    dwt_delay_us(1000);
+    if (pwm_signal < 0)
+    {
+        pwm_signal = -pwm_signal;
+        hal_tim_cc_set(PWM_FRONT_TIMER, PWM_FR_CH_CCW, pwm_signal);
+    }
+    else
+    {
+        hal_tim_cc_set(PWM_FRONT_TIMER, PWM_FR_CH_CW, pwm_signal);
+    }
+}
 
 void log_out_handler(char * buf, uint32_t len)
 {
-    drv_usart_tx(&usart2, (uint8_t *)buf, len);
+    drv_usart_tx(&vcp_usart, (uint8_t *)buf, len);
 }
 
 void wallclock_handler(void)
 {
-    int16_t enc_latest_read = (int16_t)hal_tim_count_get(ENC1_TIMER);
-    hal_tim_count_clear(ENC1_TIMER);
-
-    absolute_pos += enc_latest_read;
+    int16_t enc_latest_read = (int16_t)hal_tim_count_get(ENC_FR_TIMER);
+    hal_tim_count_clear(ENC_FR_TIMER);
 
     int16_t speed_imp_per_sec = enc_latest_read * (WALLCLOCK_TIMER_FREQ / WALLCLOCK_TIMER_TICKS);
 
-    int32_t steering_signal = pid_positional_proc(&pid1, speed_imp_per_sec);
+    int32_t steering_signal = pid_positional_proc(&pid_fr, speed_imp_per_sec);
 
-    int32_t pwm_signal = steering_signal;
-    if (pwm_signal < 0)
-    {
-        pwm_signal = -pwm_signal;
-        hal_gpio_out_set(MOTOR_DIR_PINS_PORT, MOTOR_DIR_CW_PIN);
-        hal_gpio_out_clr(MOTOR_DIR_PINS_PORT, MOTOR_DIR_CCW_PIN);
-    }
-    else
-    {
-        hal_gpio_out_clr(MOTOR_DIR_PINS_PORT, MOTOR_DIR_CW_PIN);
-        hal_gpio_out_set(MOTOR_DIR_PINS_PORT, MOTOR_DIR_CCW_PIN);
-    }
-
-    hal_tim_cc_set(PWM_TIMER, HAL_TIM_CH1, pwm_signal);
-
-    if (hal_gpio_input_check(BUTTON_PORT, BUTTON_PIN))
-    {
-        hal_gpio_out_toggle(LED_PIN_PORT, LED_ORANGE_PIN);
-    }
+    new_pwm_signal = steering_signal;
+    pid_proced = true;
 
     log_msg("Speed: %hd\n", speed_imp_per_sec);
     /*
@@ -135,8 +80,10 @@ void wallclock_handler(void)
             "Integral: %d\n\n",
             speed_imp_per_sec,
             pwm_signal,
-            pid1.integral_sum);
+            pid_fr.integral_sum);
     */
+
+    hal_gpio_out_toggle(LED_PIN_PORT, LED_PIN_ORANGE);
 
     hal_tim_evt_clear(WALLCLOCK_TIMER, HAL_TIM_EVT_UPDATE);
 }
@@ -147,11 +94,11 @@ void vcp_usart_handler(drv_usart_evt_t evt, uint8_t const * buf)
     switch(evt)
     {
         case DRV_USART_EVT_RXDONE:
-            hal_gpio_out_toggle(LED_PIN_PORT, LED_GREEN_PIN);
+            hal_gpio_out_toggle(LED_PIN_PORT, LED_PIN_GREEN);
 
             uint8_t chr = buf[0];
 
-            drv_usart_rx(&usart2, &recv_byte, 1);
+            drv_usart_rx(&vcp_usart, &recv_byte, 1);
 
             strx_mngr_feed(&strx_mngr, chr);
 
@@ -166,58 +113,59 @@ void vcp_usart_handler(drv_usart_evt_t evt, uint8_t const * buf)
 
 int main(void)
 {
-    hal_rcc_enable(VCP_PINS_RCC);
-    hal_rcc_enable(LED_PIN_RCC);
-    hal_rcc_enable(PWM_PINS_RCC);
-    hal_rcc_enable(ENC1_PINS_RCC);
+    hal_rcc_enable(HAL_RCC_GPIOA);
+    hal_rcc_enable(HAL_RCC_GPIOB);
+    hal_rcc_enable(HAL_RCC_GPIOC);
+    hal_rcc_enable(HAL_RCC_GPIOD);
+    hal_rcc_enable(HAL_RCC_GPIOE);
 
-    hal_rcc_enable(PWM_TIMER_RCC);
-    hal_rcc_enable(ENC1_TIMER_RCC);
+    hal_rcc_enable(PWM_FRONT_TIMER_RCC);
+    hal_rcc_enable(ENC_FR_TIMER_RCC);
     hal_rcc_enable(WALLCLOCK_TIMER_RCC);
 
     log_init(log_out_handler);
 
     const pid_cfg_t pidcfg =
     {
-        .kp_q = 30,
-        .ki_q = 6,
-        .kd_q = 0,
+        .kp_q = 1000,
+        .ki_q = 300,
+        .kd_q = 100,
         .q    = 8,
-        .output_min = PWM_TICKS / 2,
+        .output_min = 0,
         .output_max = PWM_TICKS,
     };
-    pid_init(&pid1, &pidcfg);
+    pid_init(&pid_fr, &pidcfg);
 
     strx_mngr_init(&strx_mngr, strx_mngr_buffer, STRX_MNGR_BUFSIZE);
 
-    hal_gpio_mode_cfg(BUTTON_PORT, BUTTON_PIN, HAL_GPIO_MODE_INPUT);
+    dwt_init();
 
-    hal_gpio_mode_cfg(LED_PIN_PORT, LED_ORANGE_PIN, HAL_GPIO_MODE_OUTPUT);
-    hal_gpio_mode_cfg(LED_PIN_PORT, LED_GREEN_PIN,  HAL_GPIO_MODE_OUTPUT);
+    hal_gpio_mode_cfg(LED_PIN_PORT, LED_PIN_ORANGE, HAL_GPIO_MODE_OUTPUT);
+    hal_gpio_mode_cfg(LED_PIN_PORT, LED_PIN_GREEN,  HAL_GPIO_MODE_OUTPUT);
 
-    hal_gpio_mode_cfg(MOTOR_DIR_PINS_PORT, MOTOR_DIR_CW_PIN,  HAL_GPIO_MODE_OUTPUT);
-    hal_gpio_mode_cfg(MOTOR_DIR_PINS_PORT, MOTOR_DIR_CCW_PIN, HAL_GPIO_MODE_OUTPUT);
+    hal_gpio_mode_cfg(VCP_PIN_PORT, VCP_PIN_TX, HAL_GPIO_MODE_AF);
+    hal_gpio_mode_cfg(VCP_PIN_PORT, VCP_PIN_RX, HAL_GPIO_MODE_AF);
+    hal_gpio_af_cfg(VCP_PIN_PORT, VCP_PIN_TX, VCP_PIN_AF);
+    hal_gpio_af_cfg(VCP_PIN_PORT, VCP_PIN_RX, VCP_PIN_AF);
 
-    hal_gpio_mode_cfg(VCP_PINS_PORT, VCP_TX_PIN, HAL_GPIO_MODE_AF);
-    hal_gpio_mode_cfg(VCP_PINS_PORT, VCP_RX_PIN, HAL_GPIO_MODE_AF);
-    hal_gpio_af_cfg(VCP_PINS_PORT,  VCP_TX_PIN,    VCP_AF_NUM);
-    hal_gpio_af_cfg(VCP_PINS_PORT,  VCP_RX_PIN,    VCP_AF_NUM);
+    hal_gpio_mode_cfg(PWM_FRONT_PIN_PORT, PWM_FR_PIN_CH_CW, HAL_GPIO_MODE_AF);
+    hal_gpio_mode_cfg(PWM_FRONT_PIN_PORT, PWM_FR_PIN_CH_CCW, HAL_GPIO_MODE_AF);
+    hal_gpio_af_cfg(PWM_FRONT_PIN_PORT, PWM_FR_PIN_CH_CW, PWM_FRONT_PIN_AF);
+    hal_gpio_af_cfg(PWM_FRONT_PIN_PORT, PWM_FR_PIN_CH_CCW, PWM_FRONT_PIN_AF);
 
-    hal_gpio_mode_cfg(PWM_PINS_PORT, PWM_CH1_PIN, HAL_GPIO_MODE_AF);
-    hal_gpio_af_cfg(PWM_PINS_PORT,  PWM_CH1_PIN,   PWM_AF_NUM);
+    hal_gpio_mode_cfg(ENC_FR_PIN_CHA_PORT, ENC_FR_PIN_CHA, HAL_GPIO_MODE_AF);
+    hal_gpio_mode_cfg(ENC_FR_PIN_CHB_PORT, ENC_FR_PIN_CHB, HAL_GPIO_MODE_AF);
+    hal_gpio_af_cfg(ENC_FR_PIN_CHA_PORT, ENC_FR_PIN_CHA, ENC_FR_PIN_AF);
+    hal_gpio_af_cfg(ENC_FR_PIN_CHB_PORT, ENC_FR_PIN_CHB, ENC_FR_PIN_AF);
 
-    hal_gpio_mode_cfg(ENC1_PINS_PORT, ENC1_CH_A_PIN, HAL_GPIO_MODE_AF);
-    hal_gpio_mode_cfg(ENC1_PINS_PORT, ENC1_CH_B_PIN, HAL_GPIO_MODE_AF);
-    hal_gpio_af_cfg(ENC1_PINS_PORT, ENC1_CH_A_PIN, ENC1_AF_NUM);
-    hal_gpio_af_cfg(ENC1_PINS_PORT, ENC1_CH_B_PIN, ENC1_AF_NUM);
+    hal_tim_timer_cfg(PWM_FRONT_TIMER, HAL_TIM_FREQ_16MHz, PWM_TICKS);
+    hal_tim_pwm_cfg(PWM_FRONT_TIMER, PWM_FR_CH_CW, HAL_TIM_PWM_POL_H1L0);
+    hal_tim_pwm_cfg(PWM_FRONT_TIMER, PWM_FR_CH_CCW, HAL_TIM_PWM_POL_H1L0);
+    hal_tim_start(PWM_FRONT_TIMER);
 
-    hal_tim_timer_cfg(PWM_TIMER, HAL_TIM_FREQ_16MHz, PWM_TICKS);
-    hal_tim_pwm_cfg(PWM_TIMER, HAL_TIM_CH1, HAL_TIM_PWM_POL_H1L0);
-    hal_tim_start(PWM_TIMER);
-
-    hal_tim_counter_cfg(ENC1_TIMER, HAL_TIM_PRESCALER_DISABLED, HAL_TIM_CAPACITY_MAXIMUM);
-    hal_tim_encoder_cfg(ENC1_TIMER, HAL_TIM_ENC_MODE3, HAL_TIM_ENC_POL_H1L0);
-    hal_tim_start(ENC1_TIMER);
+    hal_tim_counter_cfg(ENC_FR_TIMER, HAL_TIM_PRESCALER_DISABLED, HAL_TIM_CAPACITY_MAXIMUM);
+    hal_tim_encoder_cfg(ENC_FR_TIMER, HAL_TIM_ENC_MODE3, HAL_TIM_ENC_POL_H1L0);
+    hal_tim_start(ENC_FR_TIMER);
 
     NVIC_EnableIRQ(WALLCLOCK_TIMER_IRQn);
     hal_tim_timer_cfg(WALLCLOCK_TIMER, WALLCLOCK_TIMER_FREQ, WALLCLOCK_TIMER_TICKS);
@@ -226,15 +174,22 @@ int main(void)
 
     NVIC_EnableIRQ(VCP_USART_IRQn);
     drv_usart_cfg_t cfg = DRV_USART_DEFAULT_CONFIG;
-    drv_usart_init(&usart2, &cfg, vcp_usart_handler);
-    drv_usart_rx(&usart2, &recv_byte, 1);
+    drv_usart_init(&vcp_usart, &cfg, vcp_usart_handler);
+    drv_usart_rx(&vcp_usart, &recv_byte, 1);
 
     __enable_irq();
 
     char string_to_process[STR_TO_PROCESS_SIZE];
     while (1)
     {
-        if (!drv_usart_tx_ongoing_check(&usart2))
+
+        if (pid_proced)
+        {
+            pid_proced = false;
+            motor_set_pwm(new_pwm_signal);
+        }
+
+        if (!drv_usart_tx_ongoing_check(&vcp_usart))
         {
             log_process();
         }
@@ -248,7 +203,7 @@ int main(void)
 
                 log_msg("New point: %d\n", new_point);
 
-                pid_point_set(&pid1, new_point);
+                pid_point_set(&pid_fr, new_point);
             }
             else if (mini_strstartswith(string_to_process, "pidkp="))
             {
@@ -257,7 +212,7 @@ int main(void)
 
                 log_msg("New Kp: %d\n", new_kp);
 
-                pid1.kp_q = new_kp;
+                pid_fr.kp_q = new_kp;
             }
             else if (mini_strstartswith(string_to_process, "pidki="))
             {
@@ -266,7 +221,7 @@ int main(void)
 
                 log_msg("New Ki: %d\n", new_ki);
 
-                pid1.ki_q = new_ki;
+                pid_fr.ki_q = new_ki;
             }
             else if (mini_strstartswith(string_to_process, "pidkd="))
             {
@@ -275,7 +230,7 @@ int main(void)
 
                 log_msg("New Kd: %d\n", new_kd);
 
-                pid1.kd_q = new_kd;
+                pid_fr.kd_q = new_kd;
             }
         }
     }
